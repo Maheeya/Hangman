b377877cea3c1eaae10558c62f39f1e8567b7466
Maheeya4 <86717628+Maheeya4@users.noreply.github.com>
2021-08-04 10:30:15 +0100

    Add files via upload

--- Players.py
+++ Players.py
@@ -1,21 +1,149 @@
+class CheckWord:
 
-class PlayerInputs:
-    def __init__(self, p2_input):
-        self.p2_input = p2_input
+    def displayEmptyWord(self, p1):
+        word = p1
+        emptyWord = ''
+        pos = 0
+        while pos < len(word):
+            emptyWord = emptyWord + '_'
+            pos = pos + 1
+        return emptyWord
 
-    def getPlayer1Input(self):
-        p1_input = input('Player 1 please enter your chosen word: ')
-        return p1_input
+    def CheckCorrect(self, p1, p2):
+        word = p1
+        letter = p2
+        a = 0
+        Correct = 0
+        while a < len(word):
+            if word[a:a + 1] == letter:
+                Correct = 1
+            elif letter != word[a:a + 1]:
+                pass
+            a = a + 1
+        return Correct
 
-    def player2Letters(self):
-        letters = []
-        letters.append(self.p2_input)
+    def addGuess(self, guess):
+        guess = guess + 1
+        return guess
+
+    def checkAlreadyGuessed(self, p2, guess_list):
+        history = guess_list
+        pos = 0
+        alreadyGuessed = False
+        while pos < len(history):
+            if history[pos] == p2:
+                alreadyGuessed = True
+            else:
+                pass
+            pos = pos + 1
+        return alreadyGuessed
+
+    def checkString(self, p1, p2):
+        word = p1
+        letter = p2
+        pos = []
+        a = 0
+        while a < len(word):
+            if word[a:a + 1] == letter:
+                pos.append(1)
+            elif letter != word[a:a + 1]:
+                pos.append(0)
+            a = a + 1
+        return pos
+
+    def fillInMissingLetters(self, p1, p2, guess_word):
+        positions = self.checkString(p1, p2)
+        pos = 0
+        letter = p2
+        update = list(guess_word)
+        while pos < len(p1):
+            if positions[pos] == 0:
+                pass
+            elif positions[pos] == 1:
+                update[pos] = letter
+            else:
+                pass
+            pos = pos + 1
+        progressUpdate = ''.join(update)
+        return progressUpdate
 
 
-    def getPlayer2Input(self):
-        self.p2Input = input('P2 please guess a letter: ')
-        return self.p2Input
+class DrawHangman:
 
+    def hangmanStages(self, guesses):
+        if guesses == 1:
+            self.stage1()
+        elif guesses == 2:
+            self.stage2()
+        elif guesses == 3:
+            self.stage3()
+        elif guesses == 4:
+            self.stage4()
+        elif guesses == 5:
+            self.stage5()
+        elif guesses == 6:
+            self.stage6()
+        else:
+            self.stage7()
 
-class CheckWord(PlayerInputs):
-    def __init__(self):
+    def stage1(self):
+        print('__________________')
+
+    def stage2(self):
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('____________________')
+
+    def stage3(self):
+        print('__________________')
+        print('|                 |')
+        print('|                 |')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('____________________')
+
+    def stage4(self):
+        print('__________________')
+        print('|                 |')
+        print('|                 |')
+        print('|                 O')
+        print('|')
+        print('|')
+        print('|')
+        print('____________________')
+
+    def stage5(self):
+        print('__________________')
+        print('|                 |')
+        print('|                 |')
+        print('|                 O')
+        print('|                 |')
+        print('|                 |')
+        print('|')
+        print('____________________')
+
+    def stage6(self):
+        print('__________________')
+        print('|                 |')
+        print('|                 |')
+        print('|                 O')
+        print('|                \|/')
+        print('|                 |')
+        print('|')
+        print('____________________')
+
+    def stage7(self):
+        print('__________________')
+        print('|                 |  ')
+        print('|                 |  ')
+        print('|                 O  ')
+        print('|                \|/ ')
+        print('|                 |  ')
+        print('|                / \ ')
+        print('____________________ ')
--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 import Players
 
+print('hi welcome to hangman')
+p1 = input('Player 1 please type in a word: ')
+display = Players.CheckWord()
+guessWord = display.displayEmptyWord(p1)
+print(guessWord)
+guessList = []
+guesses = 0
 
-p1_input=input('Player 1 please enter your chosen word: ')
-Players.Player1(p1_input)
-Players.getPlayer2Input()
+while guessWord != p1 and guesses < 7:
 
+    p2 = input('Player 2 please guess a letter: ')
+    guess = Players.CheckWord()
+    check = guess.checkAlreadyGuessed(p2, guessList)
+    result = guess.CheckCorrect(p1, p2)
+    guessList.append(p2)
+
+    if check:
+        result = 2
+    else:
+        pass
+
+    if result == 1:
+        guessWord = guess.fillInMissingLetters(p1, p2, guessWord)
+    elif result == 0:
+        print("too bad, guess another letter: ")
+        guesses = guess.addGuess(guesses)
+        draw = Players.DrawHangman()
+        draw.hangmanStages(guesses)
+    else:
+        print(' Already guessed this letter')
+
+    print(guessWord)
